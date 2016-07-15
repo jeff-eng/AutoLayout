5fb34eeabc9babbe797a62c701caa8d7eebb0e93
Jeff Eng <jeff@jeffeng.me>
2016-07-14 17:36:41 -0700

    created labels in code and added them to the view

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -12,12 +12,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label1 = UILabel() // This creates a new instance of UILabel class
+        label1.translatesAutoresizingMaskIntoConstraints = false // Turns off auto layout constraints so we can set them manually
+        label1.backgroundColor = UIColor.redColor() // Sets BG color property by calling color method on UIColor class
+        label1.text = "THESE" // Sets label text to specified string
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.blueColor()
+        label2.text = "ARE"
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.cyanColor()
+        label3.text = "SOME"
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.greenColor()
+        label4.text = "AWESOME"
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.purpleColor()
+        label5.text = "LABELS"
+        
+        // Adds the labels created above to the view
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+       
+    }
+    
+    //Tells iOS to not display the status bar at the top
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
